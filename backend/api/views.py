c7f4c4a2a26e3c3377f47a4c83c0b3d417beace2
kadirisunday <kadirisunday212@gmail.com>
2024-10-16 15:55:06 +0100

    created CreateUserView

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+
 # Create your views here.
+class CreateUserView(generics.CreatedAPIView):
+    queryset = User.objects.all()
+    serializer_class = User
+    permission_classes = [AllowAny]
